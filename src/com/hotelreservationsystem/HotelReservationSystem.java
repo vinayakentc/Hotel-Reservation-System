4c21ace1b11996a1cf91e84e6b4c4e15dc3389d5
vinayakentc <vinayakchavanentc@gmail.com>
2021-02-26 20:47:09 +0530

    ability to find cheapest hotel for given date range

--- src/com/hotelreservationsystem/HotelReservationSystem.java
+++ src/com/hotelreservationsystem/HotelReservationSystem.java
@@ -1,20 +1,46 @@
 package com.hotelreservationsystem;
 
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class HotelReservationSystem {
 
-	static final Hotel LAKEWOOD = new Hotel("Lakewood",110);
-	static final Hotel BRIDGEWOOD = new Hotel("Bridgewood",160);
-	static final Hotel RIDGEWOOD = new Hotel("Ridgewood",220);
+	final Hotel LAKEWOOD = new Hotel("Lakewood", 110);
+	final Hotel BRIDGEWOOD = new Hotel("Bridgewood", 160);
+	final Hotel RIDGEWOOD = new Hotel("Ridgewood", 220);
+
+	static String checkInDate;
+	static String checkOutDate;
+
+	List<Hotel> hotelList = new ArrayList<>();
 	
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation Program");
-		List<Hotel> hotelList = new ArrayList<>();
-		
+	public HotelReservationSystem() {
 		hotelList.add(LAKEWOOD);
 		hotelList.add(BRIDGEWOOD);
-		hotelList.add(RIDGEWOOD);	
+		hotelList.add(RIDGEWOOD);
+	}
+
+	public int getTotalDays(String checkInDate, String checkOutDate) {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMMyyyy");
+		LocalDate date1 = LocalDate.parse(checkInDate, dtf);
+		LocalDate date2 = LocalDate.parse(checkOutDate, dtf);
+		int difference = (int) ChronoUnit.DAYS.between(date1, date2);
+		int totalDays = difference + 1;
+		return totalDays;
+	}
+
+	public String findCheapestHotelForGivenDateRangeConsideringWeekdayRatesOnly(String startDate, String endDate) {
+		int days = getTotalDays(startDate, endDate);
+		List<Integer> price = hotelList.parallelStream().map(hotel -> hotel.getWeekdayRatesForRegulars() * days)
+				.collect(Collectors.toList());
+		int minPrice = Collections.min(price);
+		Hotel cheapestHotel = hotelList.stream().filter(hotel -> hotel.getWeekdayRatesForRegulars() * days == minPrice)
+				.findFirst().orElse(null);
+		System.out.println("Cheapest hotel: " + cheapestHotel.getHotelName() + " cost: " + minPrice);
+		return cheapestHotel.getHotelName();
 	}
 }
--- src/com/hotelreservationsystem/HotelReservationTest.java
+++ src/com/hotelreservationsystem/HotelReservationTest.java
@@ -1,6 +1,14 @@
 package com.hotelreservationsystem;
 
 
-public class HotelReservationTest {	
+import org.junit.*;
 
+public class HotelReservationTest {
+	
+	@Test
+	public void findCheapestHotelForGivenDateRange_ConsideringOnlyWeekdayRates() {
+		HotelReservationSystem hotelReservationsystem = new HotelReservationSystem();
+		String hotel = hotelReservationsystem.findCheapestHotelForGivenDateRangeConsideringWeekdayRatesOnly("10Sep2020","11Sep2020");
+		Assert.assertEquals("Lakewood", hotel);
+	}
 }
